2efcc277c7e389377f5b8e4d1f891af2a76fbee1
sobith <ksobith20120021845@gmail.com>
2026-01-19 09:57:23 +0530

    my name is add to final all comit line so change

--- ind.js
+++ ind.js
@@ -1,2 +1,5 @@
 console.log('Hello sir')
 console.log("what is done for nay time second ")
+let a ="sobith kumar choudhary"
+let b ="I am a good boy but manger to asking to find maybe half to commit line ";
+console.log(`${a} is =${b}`)
