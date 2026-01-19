3e64f6ebf9e0b8546236053d1317aa973dcd63f6
sobith9818 <138216004+sobith9818@users.noreply.github.com>
2026-01-19 11:54:34 +0530

    leat commt

--- ind.js
+++ ind.js
@@ -1,4 +1,4 @@
-console.log('Hello sir')
+console.log('Hello sir the coomand by line of ')
 console.log("hello sir I am sobith kumar choudhry that bout me ")
 let a ="sobith kumar choudhary"
 let b ="I am a good boy but manger to asking to find maybe half to commit line ";
