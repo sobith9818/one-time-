0306b752bdb841aba621581f3e5941916941ed97
sobith9818 <138216004+sobith9818@users.noreply.github.com>
2026-01-19 11:29:23 +0530

    servince to commit line so chnage

--- ind.js
+++ ind.js
@@ -1,5 +1,5 @@
 console.log('Hello sir')
-console.log("what is done for nay time second ")
+console.log("hello sir I am sobith kumar choudhry that bout me ")
 let a ="sobith kumar choudhary"
 let b ="I am a good boy but manger to asking to find maybe half to commit line ";
 console.log(`${a} is =${b}`)
